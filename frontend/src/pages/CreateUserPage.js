808928f400ae7910cb359ea4c17e3db773823f63
Juhwan Son <juhwan.son@mail.utoronto.ca>
2025-12-12 02:28:40 -0500

    a

--- frontend/src/pages/CreateUserPage.js
+++ frontend/src/pages/CreateUserPage.js
@@ -14,6 +14,7 @@ const CreateUserPage = () => {
     const [error, setError] = useState('');
     const { token } = useAuth();
 
+    
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
